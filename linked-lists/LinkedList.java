21f19c30132f198d7943ac74ec1587131d64ff67
Michael Sommer <mps@mindsharemed.com>
2018-10-15 06:03:45 -0700

    Added a method.

--- linked-lists/LinkedList.java
+++ linked-lists/LinkedList.java
@@ -53,15 +53,39 @@ public class LinkedList {
 		System.out.println();
 	}
 
+	public static void printKthLast(Node head, int k) {
+		int count = 0;
+		Node node = head;
+		while (node != null) {
+			count++;
+			node = node.next;
+		}
+		count = count - k;
+		node = head;
+		while (count > 0) {
+			node = node.next;
+			count--;
+		}
+		System.out.println(node.data);
+	}
+
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.head = new Node(1);
 		list.appendToTail(2);
 		list.appendToTail(3);
 		list.appendToTail(4);
+		list.appendToTail(5);
+		list.appendToTail(6);
+		list.appendToTail(7);
+		list.appendToTail(8);
+		list.appendToTail(9);
+		list.appendToTail(10);
+
 		Node foo = list.deleteNode(8);
 		System.out.println("foo = " + foo.data);
 		printList(list.head);
-
+		System.out.println();
+		printKthLast(list.head, 3);
 	}
 }
