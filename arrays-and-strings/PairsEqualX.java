54ea66ce35ca210b9dc1f76118231dd7eccd82bd
Michael Sommer <mps@mindsharemed.com>
2018-10-15 06:04:11 -0700

    Tried to get an O(n) solution working.

--- arrays-and-strings/PairsEqualX.java
+++ arrays-and-strings/PairsEqualX.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 public class PairsEqualX {
 	public static void printPairsEqualToX(int[] arr, int X) {
 		java.util.Arrays.sort(arr);
@@ -16,6 +18,20 @@ public class PairsEqualX {
 		}
 	}
 
+	// public static void pairsEqual2(int[] arr, int X) {
+	// 	Map<Integer, Integer> map = new HashMap<>();
+	// 	for (int elem: arr) {
+	// 		if(!map.containsKey(elem)) {
+	// 			map.put(elem, 1);
+	// 		}
+	// 	}
+	// 	for (int elem: arr) {
+	// 		if (map.containsKey(X % elem)) {
+	// 			System.out.println(elem + " " + X/elem);
+	// 		}
+	// 	}
+	// }
+
 	public void printArray(int[] arr) {
 		for(int elem: arr) {
 			System.out.print(elem + " ");
@@ -26,5 +42,7 @@ public class PairsEqualX {
 		int[] arr = {12, 5, 2, 2, 8, 3, 6, 4, 1, 11};
 		int X = 12;
 		printPairsEqualToX(arr, X);
+		// System.out.println("#######");
+		// pairsEqual2(arr, X);
 	}
 }
