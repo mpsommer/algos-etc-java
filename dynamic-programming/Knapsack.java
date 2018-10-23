b8dd0bfcd1f9621976b0a6fa563e9feea85734a1
Michael Sommer <mps@mindsharemed.com>
2018-10-23 05:58:22 -0700

    Worked through knapsack

--- dynamic-programming/Knapsack.java
+++ dynamic-programming/Knapsack.java
@@ -1,28 +1,23 @@
 public class Knapsack {
 
-	// Returns the maximum value that can be put in a knapsack of capacity W
-	static int knapSack(int W, int wt[], int val[], int n) {
-		int i, w;
-		int K[][] = new int[n + 1][W + 1];
+	public static int knapSack(int W, int wt[], int val[], int n) {
+		int[][] table = new int[n+1][W+1];
 
-		// Build table K[][] in bottom up manner
-		for (i = 0; i <= n; i++) {
-			for (w = 0; w <= W; w++) {
-				K[i][w] = w;
+		for (int i = 0; i <= n; i++) {
+			for (int w = 0; w <= W; w++) {
 				if (i == 0 || w == 0) {
-					K[i][w] = 0;
-				} else if (wt[i - 1] <= w) {
-					K[i][w] = Math.max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+					table[i][w] = 0;
+				} else if(wt[i -1] <= w) {
+					table[i][w] = Math.max(val[i-1] + table[i-1][w - wt[i-1]], table[i-1][w]);
 				} else {
-					K[i][w] = K[i - 1][w];
+					table[i][w] = table[i-1][w];
 				}
-				System.out.print(K[i][w] + " ");
+				System.out.print(table[i][w] + " ");
 			}
 			System.out.println();
 		}
-		return K[n][W];
+		return table[n][W];
 	}
-
 	public static void main(String[] args) {
 		int val[] = new int[] { 60, 100, 120 };
 		int wt[] = new int[] { 1, 2, 3 };
