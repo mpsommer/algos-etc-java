1f1d753377b6bdfa95ec2b526196c354dcd5349c
Michael Sommer <mps@mindsharemed.com>
2018-10-25 06:15:29 -0700

    Worked through DIvCon.

--- maximum-sub-array/DivCon.java
+++ maximum-sub-array/DivCon.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class DivCon {
 
 	public static int[] maxCrossingSubArr(List<Integer> list, int low, int mid, int high) {
+		int[] result = new int[3];
 		int leftSum = Integer.MIN_VALUE;
 		int rightSum = Integer.MIN_VALUE;
 		int sum = 0;
-		int[] result = new int[3];
 
 		for (int i = mid; i > low; i--) {
 			sum += list.get(i);
@@ -15,9 +15,8 @@ public class DivCon {
 				result[0] = i;
 			}
 		}
-
 		sum = 0;
-		for (int j = mid + 1; j < high; j++ ) {
+		for (int j = mid + 1; j < high; j++) {
 			sum += list.get(j);
 			if (sum > rightSum) {
 				rightSum = sum;
@@ -34,25 +33,85 @@ public class DivCon {
 		int[] rightArr = new int[3];
 		int[] crossArr = new int[3];
 
-		if (high == low) {
+		if (low == high) {
 			result[0] = low;
 			result[1] = high;
 			result[2] = list.get(high);
 		} else {
-			int mid = low + (high - low) / 2;
-			leftArr = maxSubArray(list, low, mid); 
+			int mid = low + (high - low ) / 2; 
+			leftArr = maxSubArray(list, low, mid);
 			rightArr = maxSubArray(list, mid + 1, high);
 			crossArr = maxCrossingSubArr(list, low, mid, high);
+	
+			if (leftArr[2] >= rightArr[2] && leftArr[2] >= crossArr[2]) {
+				result = leftArr;
+			} else if (rightArr[2] >= leftArr[2] && rightArr[2] >= crossArr[2]) {
+				result = rightArr;
+			} else {
+				result = crossArr;
+			}
 		}
-
-		if (leftArr[2] >= rightArr[2] && leftArr[2] >= crossArr[2]) {
-			return leftArr;
-		} else if (rightArr[2] >= leftArr[2] && rightArr[2] >= crossArr[2]) {
-			return rightArr;
-		} else {
-			return crossArr;
-		}
+		return result;
 	}
+
+
+
+
+
+
+
+
+	// public static int[] maxCrossingSubArr(List<Integer> list, int low, int mid, int high) {
+	// 	int leftSum = Integer.MIN_VALUE;
+	// 	int rightSum = Integer.MIN_VALUE;
+	// 	int sum = 0;
+	// 	int[] result = new int[3];
+
+	// 	for (int i = mid; i > low; i--) {
+	// 		sum += list.get(i);
+	// 		if (sum > leftSum) {
+	// 			leftSum = sum;
+	// 			result[0] = i;
+	// 		}
+	// 	}
+
+	// 	sum = 0;
+	// 	for (int j = mid + 1; j < high; j++ ) {
+	// 		sum += list.get(j);
+	// 		if (sum > rightSum) {
+	// 			rightSum = sum;
+	// 			result[1] = j;
+	// 		}
+	// 	}
+	// 	result[2] = leftSum + rightSum;
+	// 	return result;
+	// }
+
+	// public static int[] maxSubArray(List<Integer> list, int low, int high) {
+	// 	int[] result = new int[3];
+	// 	int[] leftArr = new int[3];
+	// 	int[] rightArr = new int[3];
+	// 	int[] crossArr = new int[3];
+
+	// 	if (high == low) {
+	// 		result[0] = low;
+	// 		result[1] = high;
+	// 		result[2] = list.get(high);
+	// 	} else {
+	// 		int mid = low + (high - low) / 2;
+	// 		leftArr = maxSubArray(list, low, mid); 
+	// 		rightArr = maxSubArray(list, mid + 1, high);
+	// 		crossArr = maxCrossingSubArr(list, low, mid, high);
+	// 	}
+
+	// 	if (leftArr[2] >= rightArr[2] && leftArr[2] >= crossArr[2]) {
+	// 		return leftArr;
+	// 	} else if (rightArr[2] >= leftArr[2] && rightArr[2] >= crossArr[2]) {
+	// 		return rightArr;
+	// 	} else {
+	// 		return crossArr;
+	// 	}
+	// }
 
 	public static void printArray(int[] arr) {
 		for (int elem: arr) {
