f0874a7e1f03685d9b3a1be10192d239ea082965
Michael Sommer <mps@mindsharemed.com>
2018-10-25 05:43:29 -0700

    Worked through gcd.

--- math/GCD.java
+++ math/GCD.java
@@ -1,12 +1,17 @@
 
 public class GCD {
 
+	// public static int gcd(int a, int b) {
+	// 	if (b == 0) {return a;}
+	// 	return gcd(b, a % b);
+	// }
+
 	public static int gcd(int a, int b) {
 		if (b == 0) {return a;}
 		return gcd(b, a % b);
 	}
 	public static void main(String[] args) {
-		System.out.println("gcd of " + 98 + " and " + 56 + " = " + gcd(98, 56));
-		System.out.println("gcd of " + 7896 + " and " + 357 + " = " + gcd(7896, 357));
+		System.out.println("gcd of " + 98 + " and " + 56 + " = " + gcd(98, 56)); // 14
+		System.out.println("gcd of " + 7896 + " and " + 357 + " = " + gcd(7896, 357)); // 21
 	}
 }
