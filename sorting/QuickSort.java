13423f80bdfd3bddf84242b545f1cba4200e8d37
Michael Sommer <mps@mindsharemed.com>
2018-10-25 05:34:55 -0700

    Worked through quicksort.

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -5,24 +5,57 @@ public class QuickSort {
 	public static void quickSort(List<Integer> list, int low, int high) {
 		if (low < high) {
 			int partitionIndex = partition(list, low, high);
-			quickSort(list, low, partitionIndex -1);
+			quickSort(list, low, partitionIndex - 1);
 			quickSort(list, partitionIndex + 1, high);
 		}
 	}
 
 	public static int partition(List<Integer> list, int low, int high) {
 		int pivot = list.get(high);
-		int smallIndex = low - 1;
+		int lessIndex = low - 1;
 
-		for (int largeIndex = low; largeIndex < high; largeIndex++) {
-			if (list.get(largeIndex) <= pivot) {
-				smallIndex++;
-				Collections.swap(list, smallIndex, largeIndex);
+		for (int greaterIndex= low; greaterIndex < high; greaterIndex++) {
+			if (list.get(greaterIndex) > pivot) {
+				lessIndex++;
+				Collections.swap(list, lessIndex, greaterIndex);
 			}
 		}
-		Collections.swap(list, smallIndex + 1, high);
-		return smallIndex + 1;
+		Collections.swap(list, lessIndex + 1, high);
+		return lessIndex + 1;
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+	// public static void quickSort(List<Integer> list, int low, int high) {
+	// 	if (low < high) {
+	// 		int partitionIndex = partition(list, low, high);
+	// 		quickSort(list, low, partitionIndex -1);
+	// 		quickSort(list, partitionIndex + 1, high);
+	// 	}
+	// }
+
+	// public static int partition(List<Integer> list, int low, int high) {
+	// 	int pivot = list.get(high);
+	// 	int smallIndex = low - 1;
+
+	// 	for (int largeIndex = low; largeIndex < high; largeIndex++) {
+	// 		if (list.get(largeIndex) <= pivot) {
+	// 			smallIndex++;
+	// 			Collections.swap(list, smallIndex, largeIndex);
+	// 		}
+	// 	}
+	// 	Collections.swap(list, smallIndex + 1, high);
+	// 	return smallIndex + 1;
+	// }
 
 	public static void main(String[] args) {
 		List<Integer> arrList = new ArrayList<Integer>();
@@ -36,6 +69,14 @@ public class QuickSort {
 		arrList.add(78);
 		System.out.println(arrList);
 		quickSort(arrList, 0, arrList.size()-1);
+		System.out.println(arrList);
+		arrList = new ArrayList<Integer>();
+		arrList.add(1);
+		arrList.add(2);
+		arrList.add(3);
+		arrList.add(4);
+		System.out.println(arrList);
+		quickSort(arrList, 1, arrList.size()-1);
 		System.out.println(arrList);
 	}
 }
