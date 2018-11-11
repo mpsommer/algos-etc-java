ac3152ecb096bc0f652e860952aaa4139613116b
Michael Sommer <mps@mindsharemed.com>
2018-11-11 06:59:13 -0800

    Added some methods.

--- daily-exercise/RoutineBComplete.java
+++ daily-exercise/RoutineBComplete.java
@@ -110,6 +110,29 @@ public class RoutineBComplete {
 		}
 	}
 
+	public List<Integer> sumLevelVals(Node root) {
+		List<Integer> result = new LinkedList<>();
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			int sum = 0;
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Node node = queue.remove();
+				sum += node.val;
+				if (node.left != null) {
+					queue.add(node.left);
+				}
+				if (node.right != null) {
+					queue.add(node.right);
+				}
+			}
+			result.add(sum);
+		}
+		return result;
+	}
+
+	
 	///////////////////////////////////////////
 	////////      Matrix Traversal     ////////
 	///////////////////////////////////////////
