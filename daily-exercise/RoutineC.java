7ec708841b90f4ac0246b9afda5d8a1edef810c4
Michael Sommer <mps@mindsharemed.com>
2018-11-27 16:13:20 -0600

    Removed implementation.

--- daily-exercise/RoutineC.java
+++ daily-exercise/RoutineC.java
@@ -39,17 +39,6 @@ public class RoutineC {
 	///////////////////////////////////////////
 	public int knapSack(int W, int wt[], int val[], int n) {
 		int[][] table = new int[n+1][W+1];
-		for (int i = 0; i <= n; i++) {
-			for (int w = 0; w <= W; w++) {
-				if (i == 0 || w == 0) {
-					table[i][w] = 0;
-				} else if(wt[i -1] <= w) {
-					table[i][w] = Math.max(val[i-1] + table[i-1][w - wt[i-1]], table[i-1][w]);
-				} else {
-					table[i][w] = table[i-1][w];
-				}
-			}
-		}
 		return table[n][W];
 	}
 
