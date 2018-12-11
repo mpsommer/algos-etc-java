87a77d6a24be14c1cae3a841bc9633487c6ee430
Michael Sommer <mps@mindsharemed.com>
2018-12-10 20:23:49 -0800

    Implemented a Trie.

---
+++ daily-exercise/Trie.java
@@ -0,0 +1,30 @@
+public class Trie {
+
+	private TrieNode root;
+
+	public Trie() {
+	}
+
+	// Inserts a word into a Trie.
+	public void insert(String word) {
+	}
+
+	// Search a prefix or a whole key in trie and
+	// return the node where search ends.
+	private TrieNode searchPrefix(String word) {
+		TrieNode node = root;
+		return node;
+	}
+
+	// Returns true if the word is in the Trie
+	public boolean search(String word) {
+		return false;
+	}
+
+	public boolean startsWith(String prefix) {
+		return false;
+	}
+
+	class TrieNode {
+	}
+}
---
+++ daily-exercise/TrieComplete.java
@@ -0,0 +1,82 @@
+public class TrieComplete {
+
+	private TrieNode root;
+
+	public Trie() {
+		root = new TrieNode();
+	}
+
+	// Inserts a word into a Trie.
+	public void insert(String word) {
+		TrieNode node = root;
+		for (int i = 0; i < word.length(); i++) {
+			char currentChar = word.charAt(i);
+			if (!node.containsKey(currentChar)) {
+				node.put(currentChar, new TrieNode());
+			}
+			node = node.get(currentChar);
+		}
+		node.setEnd();
+	}
+
+	// Search a prefix or a whole key in trie and
+	// return the node where search ends.
+	private TrieNode searchPrefix(String word) {
+		TrieNode node = root;
+		for (int i = 0; i < word.length(); i++) {
+			char curLetter = word.charAt(i);
+			if (node.containsKey(curLetter)) {
+				node = node.get(curLetter);
+			} else {
+				return null;
+			}
+		}
+		return node;
+	}
+
+	// Returns true if the word is in the Trie
+	public boolean search(String word) {
+		TrieNode node = searchPrefix(word);
+		return node != null && node.isEnd();
+	}
+
+	public boolean startsWith(String prefix) {
+		TrieNode node = searchPrefix(prefix);
+		return node != null;
+	}
+
+	/**
+	 * Assumes all inputs are lowercase letters a-z. Assumes all inputs are
+	 * guarunteed to be non empty strings.
+	 */
+	class TrieNode {
+		// R links to node children
+		private TrieNode[] links;
+		private final int R = 26;
+		private boolean isEnd;
+
+		public TrieNode() {
+			links = new TrieNode[R];
+		}
+
+		public boolean containsKey(char ch) {
+			return links[ch - 'a'] != null;
+		}
+
+		public TrieNode get(char ch) {
+			return links[ch - 'a'];
+		}
+
+		public void put(char ch, TrieNode node) {
+			links[ch - 'a'] = node;
+		}
+
+		public void setEnd() {
+			isEnd = true;
+		}
+
+		public boolean isEnd() {
+			return isEnd;
+		}
+	}
+}
