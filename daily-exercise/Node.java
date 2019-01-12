f836f168ec872a89983b8515688e938f8c5299fe
Michael Sommer <mps@mindsharemed.com>
2019-01-12 17:20:35 -0600

    Tweaked the Node class for Dijkstra.

--- daily-exercise/Node.java
+++ daily-exercise/Node.java
@@ -1,10 +1,31 @@
-public class Node {
+
+import java.util.Comparator;
+class Node implements Comparator<Node> {
+	public int node;
+	public int dist;
 	int val;
 	Node left, right;
+
+	public Node() {}
 
 	public Node(int item) {
 		val = item;
 		left = null;
 		right = null;
+	}
+
+	// Used for dijkstra in routineF
+	public Node(int node, int dist) {
+		this.node = node;
+		this.dist = dist;
+	}
+	public int compare(Node node1, Node node2) {
+		if (node1.dist < node2.dist) {
+			return -1;
+		} else if (node1.dist > node2.dist) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 }
