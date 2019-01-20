b40da7e9ec38bd42bf5f914fb805fc40cd916328
Michael Sommer <mps@mindsharemed.com>
2019-01-20 07:27:09 -0600

    Removed the comparator implementatio n in the node class.

--- daily-exercise/Node.java
+++ daily-exercise/Node.java
@@ -1,6 +1,6 @@
 
 import java.util.Comparator;
-class Node implements Comparator<Node> {
+class Node {
 	public int node;
 	public int dist;
 	int val;
@@ -19,13 +19,13 @@ class Node implements Comparator<Node> {
 		this.node = node;
 		this.dist = dist;
 	}
-	public int compare(Node node1, Node node2) {
-		if (node1.dist < node2.dist) {
-			return -1;
-		} else if (node1.dist > node2.dist) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
+	// public int compare(Node node1, Node node2) {
+	// 	if (node1.dist < node2.dist) {
+	// 		return -1;
+	// 	} else if (node1.dist > node2.dist) {
+	// 		return 1;
+	// 	} else {
+	// 		return 0;
+	// 	}
+	// }
 }
