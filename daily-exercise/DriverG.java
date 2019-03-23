058b1fffe2432e25f0f79204952ad47de32a2468
Michael Sommer <mps@mindsharemed.com>
2019-03-23 08:03:11 -0500

    Addded a test for quickSelect.

--- daily-exercise/DriverG.java
+++ daily-exercise/DriverG.java
@@ -19,9 +19,19 @@ public class DriverG {
 
 		System.out.println("///// Overlapping SubProblems");
 		int n = 20;
-		System.out.println(      "fibRecursive for " + n + ": " + rtG.fibRecursive(n));
-		System.out.println(      "fibTopDown for " + n + ": " + rtG.fibTopDown(n));
-		System.out.println(      "fibBottomUp for " + n + ": " + rtG.fibBottomUp(n));
-		System.out.println(      "fibOptimal for " + n + ": " + rtG.fibOptimal(n));
+		System.out.println("fibRecursive for " + n + ": " + rtG.fibRecursive(n));
+		System.out.println("fibTopDown for " + n + ": " + rtG.fibTopDown(n));
+		System.out.println("fibBottomUp for " + n + ": " + rtG.fibBottomUp(n));
+		System.out.println("fibOptimal for " + n + ": " + rtG.fibOptimal(n));
+		System.out.println();
+		System.out.println("///// QuickSelect");
+		int[] quickArr = {10, 12, -5, -3, -1, 0, -2, 5, 8, -10, 10};
+		// int[] quickArr = {1,2,3};
+		int high = quickArr.length - 1;
+		int low = 0;
+		int target = 11;
+		int kSmallest = rtG.quickSelect(quickArr, low, high, target);
+		System.out.println("kSmallest at index: " + kSmallest);
+
 	}
 }
